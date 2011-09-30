d836669a97c718046d68841080757cb93d24b44d
Damjan Georgievski <gdamjan@gmail.com>
2011-09-30 18:08:03 +0200

    return 404 error if no document or not a paste

--- shows/paste.js
+++ shows/paste.js
@@ -1,7 +1,17 @@
-function(doc, req) {
+/*
+ * Use the mustache template in templates/paste.html to create
+ * the web page showing a single paste
+ *
+ */
+
+function (doc, req) {
+   if (!(doc && doc.title && doc.content))
+      return {"code": 404, "body": "Not found"};
+
    start({
       "Content-type": "text/html; charset='utf-8'"
    });
+
    var Mustache = require("vendor/mustache");
    Mustache.to_html(this.templates.paste, doc, null, send);
 }
